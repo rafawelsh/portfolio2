bbf02dcaa2249416205d2f09848caf8e56c96f70
rafawelsh <mrrafael1912@gmail.com>
2020-09-30 22:15:35 -0700

    first commit

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./homepage.css";
+
+function Homepage() {
+	return (
+		<div className='hero'>
+			<div className='hero-intro'>
+				<h1>Rafael Castellanos-Welsh</h1>
+				<h2>Frontend Web Developer | Graphic Design</h2>
+				<p>Current Project: Chingu Voyage 24</p>
+			</div>
+			<div className='hero-buttons'>
+				<button>PUSH ME</button>
+			</div>
+		</div>
+	);
+}
+
+export default Homepage;
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav() {
+	return (
+		<nav>
+			<h3>Logo</h3>
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>Projects</li>
+			</ul>
+		</nav>
+	);
+}
+
+export default Nav;
