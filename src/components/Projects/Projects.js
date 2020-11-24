36f552fab74afede6cf02b38a6ad7e8f30b6a1f3
rafawelsh <mrrafael1912@gmail.com>
2020-11-24 11:07:45 -0800

    fixed height issue on TallContainer within Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,20 +3,20 @@ import styled from "styled-components";
 
 // dynamically setting the height of the outermost container
 const TallContainer = styled.div.attrs(({ dynamicHeight }) => ({
-	style: { height: `${dynamicHeight}px` } || '100%',
+	style: { height: `${dynamicHeight}px` } || "100%",
 }))`
 	position: relative;
 	width: 100%;
 	background-color: var(--baseColor);
 
 	@media only screen and (max-width: 850px) {
-		max-height: 2200px
+		max-height: 2800px;
 	}
 	@media only screen and (max-device-width: 850px) {
-		height: 100%
+		height: 100%;
 	}
 	@media only screen and (min-device-width: 551px) and (max-device-width: 1024px) and (orientation: portrait) and (-webkit-min-device-pixel-ratio: 2) {
-		max-height: 100%
+		max-height: 100%;
 	}
 `;
 
@@ -27,12 +27,12 @@ const StickyContainer = styled.div`
 	height: 100vh;
 	overflow-x: hidden;
 	@media only screen and (max-width: 850px) {
-		height: 100%
+		height: 100%;
 	}
 `;
 
 const HorizontalScroll = styled.div.attrs(({ translateX }) => ({
-	style: { transform: `translateX(${translateX}px)` } || '0',
+	style: { transform: `translateX(${translateX}px)` } || "0",
 }))`
 	position: absolute;
 	height: 100%;
@@ -82,29 +82,29 @@ function Projects({ children }) {
 	return (
 		<div className='projects'>
 			{window.innerWidth >= 850 ? (
-			<TallContainer dynamicHeight={dynamicHeight} className='tallContainer'>
-				<StickyContainer ref={containerRef} className='stickyContainer'>
-					<HorizontalScroll
-						translateX={translateX}
-						ref={objectRef}
-						className='horizontalScroll'
-					>
-						{children}
-					</HorizontalScroll>
-				</StickyContainer>
-			</TallContainer>
+				<TallContainer dynamicHeight={dynamicHeight} className='tallContainer'>
+					<StickyContainer ref={containerRef} className='stickyContainer'>
+						<HorizontalScroll
+							translateX={translateX}
+							ref={objectRef}
+							className='horizontalScroll'
+						>
+							{children}
+						</HorizontalScroll>
+					</StickyContainer>
+				</TallContainer>
 			) : (
 				<TallContainer dynamicHeight='100%' className='tallContainer'>
-				<StickyContainer ref={containerRef} className='stickyContainer'>
-					<HorizontalScroll
-						translateX='0'
-						ref={objectRef}
-						className='horizontalScroll'
-					>
-						{children}
-					</HorizontalScroll>
-				</StickyContainer>
-			</TallContainer>
+					<StickyContainer ref={containerRef} className='stickyContainer'>
+						<HorizontalScroll
+							translateX='0'
+							ref={objectRef}
+							className='horizontalScroll'
+						>
+							{children}
+						</HorizontalScroll>
+					</StickyContainer>
+				</TallContainer>
 			)}
 		</div>
 	);
